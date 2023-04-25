dab9b471aed574a6b43c2313480ff77db061b22b
Listoss <129424348+Listoss@users.noreply.github.com>
2023-04-26 00:21:53 +0300

    hm_2

--- index.js
+++ index.js
@@ -1,22 +1,93 @@
-alert('Добро пожаловать!');
-alert('У меня есть 4 переменных с разными типами данных');
+//Task 1
 
-let num = 123;
-let bigNum = 1123456789n;
-let str = "JavaScript";
-let bln = false;
+let man = {
+    name: 'John',
+    age: 30
+};
 
-alert(num);
-alert(typeof(num));
+delete man.name;
+delete  man.age;
 
-alert(bigNum);
-alert(typeof(bigNum));
 
-alert(str);
-alert(typeof(str));
+//Task 2
 
-alert(bln);
-alert(typeof(bln));
+let person = {
+    name: 'Logan',
+    age: 19
+};
 
-let opinion = confirm('Понравился ли вам наш сайт?');
-alert(opinion);
+console.log('age' in person);
+
+//Task 3
+
+const student ={
+    name: 'John',
+    age: 19,
+    isHappy: true
+};
+
+for(let key in student){
+    console.log(key);
+}
+for(let key in student){
+    console.log(student[key]);
+}
+
+//Task 4
+
+const colors ={
+    'ru pum pu ru rum':{
+        red:'красный',
+        green:'зеленый',
+        blue:'синий'
+    },
+};
+
+console.log(colors['ru pum pu ru rum'].red);
+console.log(colors['ru pum pu ru rum'].blue);
+
+//Task 5
+
+let salaries ={
+    andrey: 500,
+    sveta: 413,
+    anton: 987,
+    alexey: 664,
+    alexandra: 199
+};
+
+let srZP = 0;
+let employeesNum = 0;
+
+for(let key in salaries){
+    srZP += salaries[key];
+    employeesNum++;
+}
+
+srZP=srZP/employeesNum;
+console.log('Средняя зарплата: '+srZP);
+
+//Task 6
+
+    var user = {
+	name: 'Petya',
+	age: 20,
+	isMarried: true,
+	family: undefined,
+	address: null,
+	friend: {
+		name: 'Misha'
+	}
+    };
+
+    var cloneUserObject = { writable: false,};
+
+    for(let key in user){
+        if((typeof user[key]) === 'object'){          
+            continue;
+        }
+        cloneUserObject[key] = user[key];
+        //cloneUserObject[key].writable = false;
+    }    
+    //alert('friend' in cloneUserObject);
+
