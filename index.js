4a124987d1729175994125dd8b9b140c8752b12d
Listoss <129424348+Listoss@users.noreply.github.com>
2023-05-17 17:04:01 +0300

    Calculator

--- index.js
+++ index.js
@@ -1,178 +1,105 @@
-//Task 1
+let inputStr = document.getElementById('showResultPanel');
 
-const colors = ['red', 'green', 'blue'];
-console.log(colors.length);
+let btn0 = document.getElementById('btn0');
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
+let btn4 = document.getElementById('btn4');
+let btn5 = document.getElementById('btn5');
+let btn6 = document.getElementById('btn6');
+let btn7 = document.getElementById('btn7');
+let btn8 = document.getElementById('btn8');
+let btn9 = document.getElementById('btn9');
 
-//Task 2
+let btnPlus = document.getElementById('btnPlus');
+let btnMin = document.getElementById('btnMin');
+let btnDivide = document.getElementById('btnDivide');
+let btnMultiply = document.getElementById('btnMultiply');
 
-const animals = ['monkey','dog','cat'];
-console.log(animals[animals.length-1]);
+let btnDel = document.getElementById('btnDel');
+let showResBtn = document.getElementById('showResBtn');
 
-//Task 3
-
-const numbers = [5, 43, 63, 23, 90];
-
-let firstLength = numbers.length;
-for(let i = 0; i < firstLength; i++){
-    numbers.pop();
-    console.log(numbers);
-}
-/* Variant 2
-for(let i = 0; i< firstLength; i++){
-    numbers.shift();
-    console.log(numbers);
-}
-*/ 
-
-//Task 4
-
-const students = ['Polina', 'Dasha', 'Masha'];
-
-students.pop();
-students.push('Borya');
-students.shift();
-students.unshift('Andrey');
-console.log(students);
-
-//Task 5
-
-const cats = ['Gachito', 'Tom', 'Batman'];
-
-for(let i =0; i < cats.length; i++){
-    console.log(cats[i]);
-}
-for(let k of cats){
-    console.log(k);
-}
-
-//Task 6
-
-const evenNumbers = [2, 4, 6, 8, 10];
-const oddNumbers = [1, 3, 5, 7, 9];
-const allNumbers = [];
-
-for(let i = 0; i < oddNumbers.length; i++){
-    allNumbers.push(oddNumbers[i]);
-    allNumbers.push(evenNumbers[i]);
-}
-console.log(allNumbers);
-
-for(let i = 0; i < allNumbers.length; i++){
-    if(allNumbers[i]===8){
-        console.log('index: '+ i);
-        break;
-    }
-}
-
-
-//Task 7
-
-const binary = [0, 0, 0, 0];
-let nol_Odin_Nol_Odin=[];
-let counter = 0;
-
-for(let i of binary){
-    
-    nol_Odin_Nol_Odin.push(i);
-        
-    if(counter<binary.length-1){     
-        nol_Odin_Nol_Odin.push(1);
-        counter++;
-    }
-}
-nol_Odin_Nol_Odin = nol_Odin_Nol_Odin.toString();
-console.log(nol_Odin_Nol_Odin);
-
-
-
-
-
-
-
-
-//Task 1
-
-let phrase = prompt('Введите слово для проверки на палиндром: ');
-
-function palindromChecking(array){
-    let isPalindrom = true;
-
-    if(array.length>1){
-        for(let  i = 0; i < array.length; ){
-            for(let  k = array.length-1; k > i; k--){
-                if(array[i] !== array[k]){
-                    isPalindrom = false;
-                }
-            i++;
+function findFirstNum(mathExpression, i) {
+    let firstNum='';
+    for(let k = i-1; k>= 0; k--){
+        if(Number(mathExpression[k]) || mathExpression[k]=='0'){               
+            firstNum=firstNum +mathExpression[k];
         }
-        break;
+        else if(mathExpression[k]==='+'||mathExpression[k]==='-'||mathExpression[k]==='×'||mathExpression[k]===':')
+            break;
+    }
+    firstNum = firstNum.split('').reverse().join('');
+    return firstNum;
+}
+
+function findSecondNum(mathExpression, i){
+    let secNum='';
+    for(let k = i+1; k< mathExpression.length; k++){
+        if(Number(mathExpression[k]) || mathExpression[k]=='0'){               
+            secNum=secNum + mathExpression[k];
         }
-        alert('Результат: '+ isPalindrom);
-    }
-    else{
-        alert('Длина слова слишком маленькая ');
-    }
-}
-if(phrase!==null){
-    palindromChecking(phrase);
+        else if(mathExpression[k]==='+'||mathExpression[k]==='-'||mathExpression[k]==='×'||mathExpression[k]===':')
+            break;
+    } 
+    return secNum;
 }
 
-//Task 2
 
-const matrix = [
-    [12, 98, 78, 65, 23],
-    [54, 76, 98, 43, 65],
-    [13, 324, 65, 312],
-    [9092, 22, 45, 90000],
-]
-let sum = 0;
-let elementsNum = 0;
+function countAndShowResult() {
+    let mathExpression = inputStr.value;
+    let res = 0;
 
-for(let i of  matrix){
-    for(let k of i ){
-        sum+=k;
-        elementsNum++;
+    for(let i = 0; i< mathExpression.length; i++){
+        if(mathExpression[i] === ':'){
+            let firstNum = findFirstNum(mathExpression, i);
+            let secNum= findSecondNum(mathExpression, i); 
+            res += firstNum/ secNum;     
+        }
+        else if(mathExpression[i] === '×'){
+            let firstNum = findFirstNum(mathExpression, i);
+            let secNum= findSecondNum(mathExpression, i); 
+            res += firstNum * secNum;               
+        } 
+        else if(mathExpression[i] === '-'){
+            let firstNum = findFirstNum(mathExpression, i);
+            let secNum= findSecondNum(mathExpression, i); 
+            res += firstNum - secNum;                        
+        }   
+        else if(mathExpression[i] === '+'){
+            let firstNum = findFirstNum(mathExpression, i);
+            let secNum= findSecondNum(mathExpression, i); 
+            res += Number(firstNum) + Number(secNum);                       
+        }  
     }
-    // console.log(elementsNum);
-}
-//console.log(sum);
-console.log('Среднее значение: '+ (sum/elementsNum));
-
-//Task 3
-
-const mixedNumbers = [-14, 24, -89, 43, 0 , -1, 412, 4];
-const positiveNums = [];
-const negativeNums = [];
-
-for(let i of mixedNumbers){
-    if(i<0){
-        negativeNums.push(i);
+    let resWithTabulation=' ';
+    for(let num of res.toString().split('')){
+        resWithTabulation+=num + ' ';
     }
-    else{
-        positiveNums.push(i);
-    }
-}
-console.log(positiveNums);
-console.log(negativeNums);
-
-//Task 4
-
-const randArray = [];
-const arrayElemInCube = [];
-
-for(let i = 0; i < 5; i++){
-    let x = prompt('Введите число для массива : ');
-    x = Number(x);
-    randArray.push(x);
-}
-for( let i = 0; i < randArray.length; i++){
-    let x = 1;
-
-    for(let degree = 1; degree <= 3; degree++){
-        x=x*randArray[i];
-    }
-    arrayElemInCube.push(x);
+    resWithTabulation =  resWithTabulation.slice(0, resWithTabulation.length-1);
+    inputStr.value = resWithTabulation;
 }
 
-alert(randArray);
-alert(arrayElemInCube);
+
+btn0.addEventListener('click', () => inputStr.value+=' 0');
+btn1.addEventListener('click', () => inputStr.value+=' 1');
+btn2.addEventListener('click', () => inputStr.value+=' 2');
+btn3.addEventListener('click', () => inputStr.value+=' 3');
+btn4.addEventListener('click', () => inputStr.value+=' 4');
+btn5.addEventListener('click', () => inputStr.value+=' 5');
+btn6.addEventListener('click', () => inputStr.value+=' 6');
+btn7.addEventListener('click', () => inputStr.value+=' 7');
+btn8.addEventListener('click', () => inputStr.value+=' 8');
+btn9.addEventListener('click', () => inputStr.value+=' 9');
+
+btnPlus.addEventListener('click', () => inputStr.value+=' +');
+btnMin.addEventListener('click', () => inputStr.value+=' -');
+btnDivide.addEventListener('click', () => inputStr.value+=' :');
+btnMultiply.addEventListener('click', () => inputStr.value+=' ×');
+
+showResBtn.addEventListener('click', countAndShowResult);
+
+btnDel.addEventListener('click', ()=>{ 
+    let newInputStr = inputStr.value.slice(0, inputStr.value.length -2);
+    inputStr.value = newInputStr;
+    });
+
